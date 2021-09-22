a3c06680df20f4ed85b54a59035b516df6d67412
Sam Fortuin <99059050@mydavinci.nl>
2021-09-22 11:38:23 +0200

    started on solicitatieHotelDeBotel

---
+++ solicitatieHotelDeBotel.py
@@ -0,0 +1,14 @@
+#99059050, Sam Fortuin
+
+import time
+
+#vars
+#place holder strings for list
+personCompitence = ["praktijkErvaaring", "praktijkEvaringTijd", "mbo4", "truckLicense", "ownsTopHat", ]
+personPhysical = ["name", "gender", "mustache", "hairLength", ""]
+#user input for correct compitence list info
+personCompitence[0] = input("In welk veld heeft u praktijkervaaring?\n")
+personCompitence[1] = input("Hoeveel jaar praktijkervaaring heeft u?\n")
+
+#cleanup for if statement
+#personInfo[1] = personInfo[1].lower().replace(" ","")
