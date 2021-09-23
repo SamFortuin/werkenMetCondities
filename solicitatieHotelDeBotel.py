6d2ac167d05377741279b51bffaa02acbca1a531
Sam Fortuin <99059050@mydavinci.nl>
2021-09-23 10:04:53 +0200

    added screenshot folder

--- solicitatieHotelDeBotel.py
+++ solicitatieHotelDeBotel.py
@@ -1,14 +1,15 @@
 #99059050, Sam Fortuin
-
 import time
 
 #vars
 #place holder strings for list
-personCompitence = ["praktijkErvaaring", "praktijkEvaringTijd", "mbo4", "truckLicense", "ownsTopHat", ]
-personPhysical = ["name", "gender", "mustache", "hairLength", ""]
+personCompitence = ["0_praktijkErvaaring", "1_praktijkEvaringTijd", "2_mbo4", "3_truckLicense", "4_ownsTopHat", "5_certificate"]
+personPhysical = ["0_name", "1_gender", "2_mustache", "3_hairLength", "4_height", "5_weight"]
+distractions = ["0_clownEthics"]
 #user input for correct compitence list info
-personCompitence[0] = input("In welk veld heeft u praktijkervaaring?\n")
+print("Hello wereld van…\n\npppp    y     y   ttttttt   h   h    oooo   nnnn    \np   p    y   y       t      h   h   o    o  n   n   \np   p     y y        t      h   h   o    o  n   n   \npppp       y         t      hhhhh   o    o  n   n   \np          y         t      h   h   o    o  n   n   \np          y         t      h   h   o    o  n   n   \np          y         t      h   h    oooo   n   n   \n")
+personCompitence[0] = input("In welk veld heeft u praktijkervaaring\n")
 personCompitence[1] = input("Hoeveel jaar praktijkervaaring heeft u?\n")
-
+personCompitence[2] = input("Heeft u een MBO Niveau 4 diploma?\n")
+personCompitence
 #cleanup for if statement
-#personInfo[1] = personInfo[1].lower().replace(" ","")
